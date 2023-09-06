96721289cd976c9403311d1fcaaa44a8b4eb7463
Andy Espino <65322580+Andrijo@users.noreply.github.com>
2023-09-06 11:36:52 -0600

    Create Two.java

---
+++ Paneles/Two.java
@@ -0,0 +1,19 @@
+package Paneles;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author invitado
+ */
+public class Two extends JPanel {
+    //Componentes
+    JLabel lbl_title;
+    
+    //Constructor
+    public Two() {
+
+    }
+
+}
