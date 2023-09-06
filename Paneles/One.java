b88000d72e2d24226ff8389e957e4b0b8926c850
Andy Espino <65322580+Andrijo@users.noreply.github.com>
2023-09-06 11:36:33 -0600

    Create One.java

---
+++ Paneles/One.java
@@ -0,0 +1,18 @@
+ package Paneles;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author invitado
+ */
+public class One extends JPanel {
+    //Componentes
+    JLabel lbl_title;
+    
+    //Constructor
+    public One(){
+        
+    }
+}
